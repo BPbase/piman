7c7a2febcbc82c67401a8de9f059e477557c4fff
Whiskas <Whiskas@blueplanet.com.tw>
2024-04-26 18:56:24 +0800

    update: demo html

--- vite.config.ts
+++ vite.config.ts
@@ -71,7 +71,7 @@ const buildPackageConfig = {
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
 
-  if (command === 'production-package') {
+  if (mode === 'production-package') {
     return buildPackageConfig
   } else {
     return buildDemoConfig
