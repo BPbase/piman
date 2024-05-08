0484ab6baf642a69e90927975578b5a942a105f4
Neil Lin <neillin@blueplanet.com.tw>
2024-05-08 17:00:03 +0800

    feat: menu add group style

--- src/views/DemoHome.vue
+++ src/views/DemoHome.vue
@@ -10,22 +10,8 @@
           <div>無障礙前端 UI 框架</div>
         </router-link>
       </div>
-      <!-- <div class="aside-block">
-        <bpa-select
-          id="lang"
-          v-model="value"
-          :options="langOptions"
-          size="small"
-          disabledClear
-          style="width: 100%;"
-        >
-          <template v-slot:prefix>
-            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="language" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="svg-inline--fa fa-language fa-w-20 fa-2x"><path fill="currentColor" d="M152.1 236.2c-3.5-12.1-7.8-33.2-7.8-33.2h-.5s-4.3 21.1-7.8 33.2l-11.1 37.5H163zM616 96H336v320h280c13.3 0 24-10.7 24-24V120c0-13.3-10.7-24-24-24zm-24 120c0 6.6-5.4 12-12 12h-11.4c-6.9 23.6-21.7 47.4-42.7 69.9 8.4 6.4 17.1 12.5 26.1 18 5.5 3.4 7.3 10.5 4.1 16.2l-7.9 13.9c-3.4 5.9-10.9 7.8-16.7 4.3-12.6-7.8-24.5-16.1-35.4-24.9-10.9 8.7-22.7 17.1-35.4 24.9-5.8 3.5-13.3 1.6-16.7-4.3l-7.9-13.9c-3.2-5.6-1.4-12.8 4.2-16.2 9.3-5.7 18-11.7 26.1-18-7.9-8.4-14.9-17-21-25.7-4-5.7-2.2-13.6 3.7-17.1l6.5-3.9 7.3-4.3c5.4-3.2 12.4-1.7 16 3.4 5 7 10.8 14 17.4 20.9 13.5-14.2 23.8-28.9 30-43.2H412c-6.6 0-12-5.4-12-12v-16c0-6.6 5.4-12 12-12h64v-16c0-6.6 5.4-12 12-12h16c6.6 0 12 5.4 12 12v16h64c6.6 0 12 5.4 12 12zM0 120v272c0 13.3 10.7 24 24 24h280V96H24c-13.3 0-24 10.7-24 24zm58.9 216.1L116.4 167c1.7-4.9 6.2-8.1 11.4-8.1h32.5c5.1 0 9.7 3.3 11.4 8.1l57.5 169.1c2.6 7.8-3.1 15.9-11.4 15.9h-22.9a12 12 0 0 1-11.5-8.6l-9.4-31.9h-60.2l-9.1 31.8c-1.5 5.1-6.2 8.7-11.5 8.7H70.3c-8.2 0-14-8.1-11.4-15.9z" class=""></path></svg>
-          </template>
-        </bpa-select>
-      </div> -->
       <div class="aside-block">
-        <nav class="menu">
+        <nav class="menu" aria-label="元件選單">
           <ul>
             <li><router-link to="/accordion">Accordion</router-link></li>
             <li><router-link to="/breadcrumb">Breadcrumb</router-link></li>
@@ -49,14 +35,17 @@
         </nav>
       </div>
       <div class="aside-block">
-        <nav class="menu">
+        <nav class="menu" aria-label="資源與連結">
+          <div class="aside-group">Coding</div>
           <ul>
             <li>
-              <a href="https://github.com/bpbase/piman" target="_blank" rel="noreferrer noopener">Github</a>
+              <a href="https://github.com/bpbase/piman" target="_blank" rel="noreferrer noopener" title="open new window to Github">Github</a>
             </li>
-            <!-- <li>
-              <a href="https://piman.cc/category/document/components/" target="_blank" rel="noreferrer noopener">Docs</a>
-            </li> -->
+          </ul>
+          <ul>
+            <li>
+              <a href="https://bpio.gitbook.io/piman/" target="_blank" rel="noreferrer noopener" title="open new window to Docs">Docs</a>
+            </li>
           </ul>
         </nav>
       </div>
@@ -101,13 +90,6 @@ section {
 svg {
   margin-right: 0.25rem;
 }
-/* #bpa-select-lang{
-  width: 200px;
-  svg {
-    width: 32px;
-    height: 32px;
-  }
-} */
 .logo {
   display: block;
   text-align: center;
@@ -155,7 +137,7 @@ aside {
     display: flex;
     align-items: center;
   }
-  & a {
+  a {
     color: white;
     &:visited {
       color: #FFBB00;
@@ -166,22 +148,29 @@ aside {
   padding: 1rem 1.5rem;
   border-bottom: 1px solid rgba(255, 255, 255, 0.2);
   @media screen and (width <= 768px) {
-    flex: 1;
+    /* flex: 1; */
     border-bottom-width: 0;
   }
 }
 .menu {
-  & ul {
+  ul {
     margin-top: -1rem;
     margin-left: -1.5rem;
     margin-right: -1.5rem;
     @media screen and (width <= 768px) {
-      display: flex;
       margin-top: 0;
+      margin-left: 0;
+      margin-right: 0;
+      display: inline-block;
+      vertical-align: middle;
     }
   }
-  & li {
-    & a {
+  li {
+    @media screen and (width <= 768px) {
+      display: inline-block;
+      vertical-align: middle;
+    }
+    a {
       display: block;
       padding: 1rem 1.5rem;
       &:hover {
@@ -196,6 +185,38 @@ aside {
       }
     }
   }
+  &:first-child {
+    &:has(.aside-group) {
+      margin-top: -1rem;
+      @media screen and (width <= 768px) {
+        margin-top: 0;
+        margin-left: -1rem;
+      }
+      ul {
+        margin-top: 0;
+      }
+    }
+  }
+  @media screen and (width <= 768px) {
+    margin: 0;
+  }
+}
+.aside-group {
+  background-color: oklch(100% 0 0 / 0.5);
+  color: oklch(var(--color-black));
+  font-size: 0.75rem;
+  padding: var(--spacing-xxs) var(--spacing-l);
+  margin-left: calc(0px - var(--spacing-l));
+  margin-top: 0;
+  width: calc(100% + 2 * (var(--spacing-l)));
+  @media screen and (width <= 768px) {
+    margin: 0;
+    width: initial;
+    padding: var(--spacing-xxs) var(--spacing-xxs);
+    display: inline-block;
+    vertical-align: middle;
+    border-radius: var(--radius);
+  }
 }
 main {
   flex: 1;
@@ -206,18 +227,18 @@ main {
   gap: 1rem;
   padding: 1rem;
   color: white;
-  & button {
+  button {
     width: 3rem;
     height: 3rem;
     display: flex;
     align-items: center;
   }
-  & svg {
+  svg {
     width: 1rem;
     height: 1rem;
     margin: 0;
   }
-  & a {
+  a {
     color: white;
     &:visited {
       color: #FFBB00;
@@ -231,6 +252,7 @@ main {
 }
 .page-main {
   min-height: 100vh;
+  word-break: break-word;
   padding: 1rem 2rem;
   background-color: #f2f2f2;
   border-top-left-radius: 0.75rem;
