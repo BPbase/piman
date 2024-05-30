c89d78b065e1198e3fcb8a0a834514ff3dbca5c1
Whiskas <Whiskas@blueplanet.com.tw>
2024-05-30 14:32:03 +0800

    refactor: tabs component eslint

--- src/components/Tabs/PiTabPanel.vue
+++ src/components/Tabs/PiTabPanel.vue
@@ -1,23 +1,22 @@
 <template>
-<div
-  :id="`tab-panel-${name}`"
-  role="tabpanel"
-  v-show="showPanel"
-  :aria-labelledby="`tab-${name}`"
-  tabindex="0"
-  :class="[
-    'pi-tabs-panel',
-    showPanel ? 'pi-tabs-panel--selected': '',
-    nopadding ? 'no-padding' : ''
-  ]"
->
-  <slot></slot>
-</div>
+  <div
+    :id="`tab-panel-${name}`"
+    role="tabpanel"
+    v-show="showPanel"
+    :aria-labelledby="`tab-${name}`"
+    tabindex="0"
+    :class="[
+      'pi-tabs-panel',
+      showPanel ? 'pi-tabs-panel--selected' : '',
+      nopadding ? 'no-padding' : ''
+    ]"
+  >
+    <slot></slot>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, getCurrentInstance } from 'vue';
-
+import { computed, getCurrentInstance } from 'vue'
 
 const props = defineProps({
   name: {
@@ -27,18 +26,18 @@ const props = defineProps({
   prefixIcon: String,
   label: {
     type: String,
-    required: true,
+    required: true
   },
   affixIcon: String,
   badge: {
-    type: String || Number,
+    type: String || Number
   },
   nopadding: Boolean
 })
 
 const showPanel = computed(() => {
   const self = getCurrentInstance()
-  if(!self || !self.parent) return false
+  if (!self || !self.parent) return false
   return props.name === self.parent.props.modelValue
 })
 </script>
--- src/components/Tabs/PiTabs.vue
+++ src/components/Tabs/PiTabs.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="pi-tabs">
-    <ul
-      role="tablist"
-      class="pi-tabs-list"
-    >
+    <ul role="tablist" class="pi-tabs-list">
       <li
         role="presentation"
         v-for="(tab, index) in tabs"
         :key="index"
-        :class="[
-          'pi-tabs__item',
-          tab.name === modelValue ? 'pi-tabs__item--selected' : ''
-        ]"
+        :class="['pi-tabs__item', tab.name === modelValue ? 'pi-tabs__item--selected' : '']"
       >
         <pi-button
           role="tab"
@@ -21,16 +15,16 @@
           :tabindex="tab.name === modelValue ? '0' : '-1'"
           :aria-controls="`tab-panel-${toKebabCase(tab.name)}`"
           :aria-selected="tab.name === modelValue ? true : false"
-          :class="[
-            tab.name === modelValue ? 'pi-btn--primary' : ''
-          ]"
+          :class="[tab.name === modelValue ? 'pi-btn--primary' : '']"
           @click="handleClickTab(tab.name, index)"
           @keydown="handleKeyboardEvent($event)"
         >
-          <span v-if="tab.prefix"  :class="['tab-prefix', tab.prefix]"></span>
+          <span v-if="tab.prefix" :class="['tab-prefix', tab.prefix]"></span>
           <span>{{ tab.label }}</span>
           <span v-if="tab.affix" :class="['tab-affix', tab.affix]"></span>
-          <span v-if="tab.badge" class="pi-badge" :aria-label="t('badge.pre') + tab.badge ">{{ tab.badge }}</span>
+          <span v-if="tab.badge" class="pi-badge" :aria-label="t('badge.pre') + tab.badge">{{
+            tab.badge
+          }}</span>
         </pi-button>
       </li>
     </ul>
@@ -41,8 +35,8 @@
 </template>
 
 <script lang="ts" setup>
-import useI18n from '@/locales/useI18n';
-import { computed, getCurrentInstance, ref } from 'vue';
+import useI18n from '@/locales/useI18n'
+import { computed, getCurrentInstance, ref } from 'vue'
 
 const props = defineProps({
   modelValue: String
@@ -50,61 +44,63 @@ const props = defineProps({
 
 const { t } = useI18n()
 
-const emits = defineEmits(["update:modelValue"])
+const emits = defineEmits(['update:modelValue'])
 
-const tabs = computed(()=>{
+const tabs = computed(() => {
   const self = getCurrentInstance()
-  if(!self || !self.slots.default) return []
-  return self.slots.default().map( kid => kid.props || {} )
+  if (!self || !self.slots.default) return []
+  return self.slots.default().map((kid) => kid.props || {})
 })
 const currentIndex = ref(0)
 
-
-const handleClickTab = (name: string, index:number) => {
-  emits("update:modelValue", name);
-  currentIndex.value = index;
+const handleClickTab = (name: string, index: number) => {
+  emits('update:modelValue', name)
+  currentIndex.value = index
 }
 
-const handleKeyboardEvent = (e:any) => {
-  if(e.key == 'ArrowLeft'   || e.keyCode == 37) { handleClickPrevTab() }
-  if(e.key == 'ArrowUp'     || e.keyCode == 38) { handleClickPrevTab() }
-  if(e.key == 'ArrowRight'  || e.keyCode == 39) { handleClickNextTab() }
-  if(e.key == 'ArrowDown'   || e.keyCode == 40) { handleClickNextTab() }
+const handleKeyboardEvent = (e: any) => {
+  if (e.key == 'ArrowLeft' || e.keyCode == 37) {
+    handleClickPrevTab()
+  }
+  if (e.key == 'ArrowUp' || e.keyCode == 38) {
+    handleClickPrevTab()
+  }
+  if (e.key == 'ArrowRight' || e.keyCode == 39) {
+    handleClickNextTab()
+  }
+  if (e.key == 'ArrowDown' || e.keyCode == 40) {
+    handleClickNextTab()
+  }
 }
 
 const handleClickPrevTab = () => {
-  currentIndex.value -= 1;
+  currentIndex.value -= 1
   if (currentIndex.value === -1) {
-    currentIndex.value = tabs.value.length - 1;
+    currentIndex.value = tabs.value.length - 1
   }
-  (document as any)
-    .getElementById(
-      "tab-" + toKebabCase(tabs.value[currentIndex.value]["name"])
-    )
-    .focus();
-  emits("update:modelValue", tabs.value[currentIndex.value]["name"]);
+  ;(document as any)
+    .getElementById('tab-' + toKebabCase(tabs.value[currentIndex.value]['name']))
+    .focus()
+  emits('update:modelValue', tabs.value[currentIndex.value]['name'])
 }
 
 const handleClickNextTab = () => {
-  currentIndex.value += 1;
+  currentIndex.value += 1
   if (currentIndex.value === tabs.value.length) {
-    currentIndex.value = 0;
+    currentIndex.value = 0
   }
-  (document as any)
-    .getElementById(
-      "tab-" + toKebabCase(tabs.value[currentIndex.value]["name"])
-    )
-    .focus();
-  emits("update:modelValue", tabs.value[currentIndex.value]["name"]);
+  ;(document as any)
+    .getElementById('tab-' + toKebabCase(tabs.value[currentIndex.value]['name']))
+    .focus()
+  emits('update:modelValue', tabs.value[currentIndex.value]['name'])
 }
 
-const toKebabCase = (value:string) => {
-  value = value.toLowerCase();
-  value = value.replace(/^\s+/g, "");
-  value = value.replace(/\s+$/g, "");
-  return value.replace(/\s+/, "-");
+const toKebabCase = (value: string) => {
+  value = value.toLowerCase()
+  value = value.replace(/^\s+/g, '')
+  value = value.replace(/\s+$/g, '')
+  return value.replace(/\s+/, '-')
 }
-
 </script>
 
 <style scoped>
@@ -143,7 +139,7 @@ const toKebabCase = (value:string) => {
         }
       }
     }
-    
+
     &.pi-tabs__item--selected {
       position: relative;
       z-index: 1;
